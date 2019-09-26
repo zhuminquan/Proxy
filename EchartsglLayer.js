818c81d1faad06339ceb19206d0fc675e877dc7e
zhumq <2213052498@qq.com>
2019-09-26 21:40:01 +0800

    initsss

--- EchartsglLayer.js
+++ EchartsglLayer.js
@@ -1,9 +1,7 @@
 define(['dojo/dom'], function(dom) {
   return {
     setStyle: function(obj) {
-      obj.style.backgroundColor = 'pink';
-      obj.style.width = '200px';
-      obj.style.height = '200px';
+      console.log(obj);
     }
   };
 });
