936ad74346f16695bc88a007657dd1b5bb876f58
zhumq <2213052498@qq.com>
2019-09-26 21:29:35 +0800

    init project

---
+++ EchartsglLayer.js
@@ -0,0 +1,8 @@
+define(['dojo/_base/declare'], function (declare) {
+  return declare('EchartsglLayer', null, {
+    name: 'EchartsglLayer',
+    constructor: function () {
+      console.log('终于进来了')
+    }
+  })
+})
